211202a4721625251d84f1de69cb19e2424ba7b9
rolandaayo <ibiwoyeroland@gmail.com>
2025-04-24 20:49:46 +0100

    post route & readme

---
+++ README.md
@@ -0,0 +1,53 @@
+<div align="center">
+
+  <h2 align="center">Natours</h2>
+
+
+
+ Natours is a modern and visually appealing web application designed for booking tours. It features user authentication and full CRUD operations. <br/> Built using Node.js, Express.js - A learning project for backend development. <br/>
+
+  <a href="#"><strong>➥ Live Demo</strong></a>
+
+</div>
+
+<br />
+
+### Demo Screeshots
+
+![Micro Desktop Demo](./readme-images/readme-1.png "Desktop Demo")
+
+### Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+* [Git](https://git-scm.com/downloads "Download Git") must be installed on your operating system.
+
+
+### Installing Natours
+
+
+To install **Natours**, run this command on your git bash:
+
+Linux and macOS:
+
+```bash
+
+sudo git clone https://github.com/rolandaayo/natours.git
+```
+
+Windows:
+
+```bash
+
+git clone https://github.com/rolandaayo/natours.git
+```
+
+## 💬 Contact
+
+If you have an offer, opportunity, or introduction that might make my life more interesting, email me at ibiwoyeroland@gmail.com.
+
+For the reasons stated above, I'll only respond to proposals that are matched for my schedule and interests.
+
+### License
+
+This project is **free to use** and does not contains any license.
--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 const express = require("express");
 
 const app = express();
-app.use(express.json()); //middleware
+app.use(express.json()); // for parsing application/ serves as middleware
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get("/api/v1/tours", (req, res) => { //get request - to get all information
+app.get("/api/v1/tours", (req, res) => { // Handles GET request to retrieve all tours
     res.status(200).json({
         status: "success",
         results: tours.length,
@@ -18,9 +18,23 @@ app.get("/api/v1/tours", (req, res) => { //get request - to get all information
     })
 });
 
-app.post("/api/v1/tours", (req, res) => { // post request - to add new data
-    console.log(req.body);
-    res.send("Done!");
+app.get("/api/v1/tours/:id", (req, res) => { // Handles Get request to retrieve a single tour
+
+})
+
+app.post("/api/v1/tours", (req, res) => { // Handles POST request to create a new tour
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err => {
+        res.status(201).json({ 
+            status: "success", 
+            data: {
+                tour: newTour
+            }   
+        })
+    })
+    res.send("Added new tour!");
 });
 
 const port = 3000;
